e3609cf228b45874c90eb9a7ed1601820870a537
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2024-02-23 11:39:40 +0200

    added GET API requests

---
+++ src/js/api.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+
+// Перелік категорій кніг
+async function getCategoryList() {
+  const categoryList = await axios.get(`https://books-backend.p.goit.global/books/category-list`);
+  return categoryList;
+};
+
+// Популярні книги, що належать до усіх категорій
+async function getTopBooks() {
+  const topBooks = await axios.get(`https://books-backend.p.goit.global/books/top-books`);
+  return topBooks;
+};
+
+// Книги окремої категорії
+async function getByCategoryBooks(paramCategory) {
+  const categoryBooks = await axios.get(`https://books-backend.p.goit.global/books/category?category=${paramCategory}`);
+  return categoryBooks;
+};
+
+// Детальна інформація про книгу
+async function getByIdBook(paramId) {
+  const idBook = await axios.get(`https://books-backend.p.goit.global/books/${paramId}`);
+  return idBook;
+};
+
+
+export { getCategoryList, getTopBooks, getByCategoryBooks, getByIdBook };
