f2a1e2be6d9cb15cd250d437efda8436b8c990cc
Данил <danikkach666@gmail.com>
2024-02-24 21:03:21 +0200

    add corect scroll

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,22 +1,14 @@
-// import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
 
-// import styles bundle
 import 'swiper/css/bundle';
 
-// init Swiper:
-// const swiper = document.querySelector('swiper');
-
-const swiper = new Swiper('swiper', {
-  // Navigation arrows
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 6,
+  spaceBetween: 20,
+  direction: 'vertical',
+  speed: 400,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  keyboard: true,
-  mousewhell: true,
-  direction: 'vertical',
 });
-
-// Now you can use all slider methods like
-swiper.slideNext();
