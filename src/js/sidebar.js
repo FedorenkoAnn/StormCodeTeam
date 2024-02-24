0a9509cd2697434b0d45ab228b1660287263ff7f
Данил <danikkach666@gmail.com>
2024-02-24 13:06:47 +0200

    add

---
+++ src/js/sidebar.js
@@ -0,0 +1,19 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  direction: 'vertical',
+});
+
+// Now you can use all slider methods like
+swiper.slideNext();
