f94f42c8ea4d5b2272f1359b4855a3b5084aa597
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-02-26 14:34:21 +0100

    Updating scroll-button with js

---
+++ src/js/scrollup-button.js
@@ -0,0 +1,27 @@
+import throttle from 'lodash.throttle';
+
+const scrollUpBtnEl = document.querySelector(".scroll-up");
+scrollUpBtnEl.addEventListener('click', onScrollUpClick);
+
+function onScrollUpClick() {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });  
+}
+ window.addEventListener('scroll', throttle(showScrollUpButton,1000));
+ 
+function showScrollUpButton(event) {
+
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+   scrollY > 2000 ? showScrollUpBtn() : hideScrollUpBtn(); 
+}
+
+function hideScrollUpBtn() {
+ scrollUpBtnEl.classList.add("is-hidden") 
+}
+
+function showScrollUpBtn() {
+  scrollUpBtnEl.classList.remove("is-hidden");
+}
