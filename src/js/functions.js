a08135a17c6bd56dd8a1502f8f96b039552f819c
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2024-02-27 09:41:39 +0200

    tablet and desktop versions

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,3 +1,14 @@
 // Best Sellers Books
+import {viewport} from "./variables.js";
 
-
+export const bookCount = () => {
+  if(viewport < 768){
+    return 1;
+  }else
+  if(viewport >= 768 && viewport < 1440 ){
+    return 3;
+  }else
+  if(viewport > 1440){
+    return 5;
+  }
+}
--- src/js/markup.js
+++ src/js/markup.js
@@ -1,12 +1,14 @@
 // Best Sellers Books
-// import {viewport} from "./variables.js" 
+
+import {bookCount} from "./functions.js";
 export const createGalleryTopBooks = (arr) => {
   return `<h1 class="books_galerry-title">Best Sellers <span class="books_galerry-title-subject">Books</span></h1>
-  ${arr.map(item => {const markup = `
+  ${arr.map(item => {
+    const markup = `
     <h2 class="books_card-heading js-heading">${item.list_name}</h2>
-        ${item.books.map(({ _id, book_image, title, author }) => {
-          return  `<ul class="book_card-list" >
-                     <li class="book_card-item js-card" id=${_id}>
+    <ul class="book_card-list" >
+        ${item.books.slice(0, bookCount()).map(({ _id, book_image, title, author }) => {
+         return `<li class="book_card-item js-card" id=${_id}>
                        <img
                          class="book_card_photo"
                          src=${book_image}
@@ -14,39 +16,15 @@ export const createGalleryTopBooks = (arr) => {
                        />
                         <h3 class="book_card-title">${title}</h3>
                         <h4 class="book_card_author">${author}</h4>
-                     </li>
-                   </ul>`  
+                     </li>`
         })
-        }
-        <div class"btn_more">
+      }
+      </ul>
+        <div class="btn_more">
     <button class="btn_see-more">See more</button>
     </div>`
     return markup
   })
-}
+    }
 `
 }
-//   return arr.map(item => {
-//     const markup = `
-//     <h1 class="books_galerry">Best Sellers Books</h1>
-//     <h2 class="books_card-heading js-heading">${item.list_name}</h2>
-     
-//         ${item.books.map(({ _id, book_image, title, author }) => {
-//           return  `<ul class="book_card-list" >
-//                      <li class="book_card-item js-card" id=${_id}>
-//                        <img
-//                          class="book_card_photo"
-//                          src=${book_image}
-//                          alt= book cover photo ${title}
-//                        />
-//                         <h3 class="book_card-title">${title}</h3>
-//                         <h4 class="book_card_author">${author}</h4>
-//                      </li>
-//                    </ul>`  
-//         })
-//         }
-//     <btn class="btn_see-more">See more</btn>
-//     </section> `
-//     return markup
-//   })
-// }
