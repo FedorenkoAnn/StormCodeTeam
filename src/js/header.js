9ae957728703a57117d51e4e055bac562fa33f65
Данил <danikkach666@gmail.com>
2024-02-28 01:58:48 +0200

    add complete shopping-list

--- src/js/header.js
+++ src/js/header.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  const navLinks = document.querySelectorAll('.link-menu');
+  const navLinks = document.querySelectorAll('#link-menu');
 
   navLinks.forEach(function (link) {
     link.addEventListener('click', function (event) {
@@ -42,25 +42,25 @@ function toggleModal(action) {
   }
 }
 
-// document.querySelector('.burger-btn').addEventListener('click', function () {
-//   toggleModal('open');
-// });
+document.querySelector('.burger-btn').addEventListener('click', function () {
+  toggleModal('open');
+});
 
-// document.querySelector('.close-btn').addEventListener('click', function () {
-//   toggleModal('close');
-// });
+document.querySelector('.close-btn').addEventListener('click', function () {
+  toggleModal('close');
+});
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   const homeLink = document.querySelector('.nav-list a[href="../index.html"]');
-//   const shoppingListLink = document.querySelector('.js-shopping-list a');
+document.addEventListener('DOMContentLoaded', function () {
+  const homeLink = document.querySelector('.nav-list a[href="../index.html"]');
+  const shoppingListLink = document.querySelector('.js-shopping-list a');
 
-//   homeLink.addEventListener('click', function(event) {
-//       event.preventDefault();
-//       window.location.href = this.getAttribute('href');
-//   });
+  homeLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = this.getAttribute('href');
+  });
 
-//   shoppingListLink.addEventListener('click', function(event) {
-//       event.preventDefault();
-//       window.location.href = this.getAttribute('href');
-//   });
-// });
+  shoppingListLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = this.getAttribute('href');
+  });
+});
