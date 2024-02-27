12760ba5ae5709fdfa34b0b2e37b323151b6cc0d
Maria Khaklenko <maria.khalenko@gmail.com>
2024-02-28 00:23:45 +0200

    add header

--- src/js/header.js
+++ src/js/header.js
@@ -13,21 +13,21 @@
   });
 
 
-  document.addEventListener("DOMContentLoaded", function() {
-       const navLinks = document.querySelectorAll('#link-menu');
+  // document.addEventListener("DOMContentLoaded", function() {
+  //      const navLinks = document.querySelectorAll('#link-menu');
 
-       navLinks.forEach(function(link) {
-      link.addEventListener('click', function(event) {
-      event.preventDefault();
+  //      navLinks.forEach(function(link) {
+  //     link.addEventListener('click', function(event) {
+  //     event.preventDefault();
 
-      navLinks.forEach(function(link) {
-      link.classList.remove('is-active');
-   });
+  //     navLinks.forEach(function(link) {
+  //     link.classList.remove('is-active');
+  //  });
 
-      link.classList.add('is-active');
-      });
-    });
-  });
+  //     link.classList.add('is-active');
+  //     });
+  //   });
+  // });
   
 
 
@@ -55,17 +55,17 @@ document.querySelector('.close-btn').addEventListener('click', function() {
   toggleModal('close');
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  const homeLink = document.querySelector('.nav-list a[href="../index.html"]');
-  const shoppingListLink = document.querySelector('.js-shopping-list a');
+// document.addEventListener("DOMContentLoaded", function() {
+//   const homeLink = document.querySelector('.nav-list a[href="../index.html"]');
+//   const shoppingListLink = document.querySelector('.js-shopping-list a');
 
-  homeLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      window.location.href = this.getAttribute('href');
-  });
+//   homeLink.addEventListener('click', function(event) {
+//       event.preventDefault();
+//       window.location.href = this.getAttribute('href');
+//   });
 
-  shoppingListLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      window.location.href = this.getAttribute('href');
-  });
-});
+//   shoppingListLink.addEventListener('click', function(event) {
+//       event.preventDefault();
+//       window.location.href = this.getAttribute('href');
+//   });
+// });
