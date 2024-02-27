11079099da8c9c0fcd3070ee0cf9967238415a24
Maria Khaklenko <maria.khalenko@gmail.com>
2024-02-27 17:17:39 +0200

    header-mob, tab, desk

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,5 @@
-  document.addEventListener('DOMContentLoaded', function () {
+
+ document.addEventListener('DOMContentLoaded', function () {
     var toggleSwitch = document.querySelector('.switch input');
 
     toggleSwitch.addEventListener('change', function () {
@@ -10,13 +11,46 @@
           
     });
   });
- var navLinks = document.querySelectorAll('.nav-link');
-   navLinks.forEach(function (navLink) {
-      navLink.addEventListener('click', function () {
-        navLinks.forEach(function (link) {
-          link.classList.remove('is-active');
-        });
 
-        navLink.classList.add('is-active');
+
+  document.addEventListener("DOMContentLoaded", function() {
+       var navLinks = document.querySelectorAll('#link-menu');
+
+       navLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      navLinks.forEach(function(link) {
+      link.classList.remove('is-active');
+   });
+
+      link.classList.add('is-active');
       });
     });
+  });
+  
+
+
+function toggleModal(action) {
+  var mobileContent = document.querySelector('.mobile-content');
+  var burgerBtn = document.querySelector('.burger-btn');
+  var closeBtn = document.querySelector('.close-btn');
+
+  if (action === 'open') {
+    mobileContent.style.display = 'block';
+    burgerBtn.style.display = 'none';
+    closeBtn.style.display = 'block';
+  } else {
+    mobileContent.style.display = 'none';
+    burgerBtn.style.display = 'block';
+    closeBtn.style.display = 'none';
+  }
+}
+
+document.querySelector('.burger-btn').addEventListener('click', function() {
+  toggleModal('open');
+});
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+  toggleModal('close');
+});
