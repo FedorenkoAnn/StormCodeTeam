6c6a59ae603849328963e7b7aec903637c7fed98
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-02-28 02:43:46 +0200

    added-object-to-LS

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,8 @@
 import { getBookById } from './api';
 import amazon1x from '../img/amazon@1x.png';
-import amazon2x from '../img/amazon@2x.png';
+// import amazon2x from '../img/amazon@2x.png';
 import app_books1x from '../img/app_books@1x.png';
-import app_books2x from '../img/app_books@2x.png';
+// import app_books2x from '../img/app_books@2x.png';
 const modalBodyEl = document.querySelector('.modal-body');
 const backdrop = document.querySelector('.backdrop');
 const modalCloseBtn = document.querySelector('.modal-close-btn');
@@ -12,6 +12,7 @@ const savedShoppingList = localStorage.getItem('shoppingList');
 if (savedShoppingList) {
   shoppingList = JSON.parse(savedShoppingList);
 }
+
 modalCloseBtn.addEventListener('click', closeModal);
 document.addEventListener('click', async event => {
   try {
@@ -22,16 +23,18 @@ document.addEventListener('click', async event => {
       const id = hardcoverItem.id;
       const response = await getBookById(id);
       const bookData = response.data;
-      const markup = markupBook(bookData);
+      const truncatedBookData = {
+        _id: bookData._id,
+        book_image: bookData.book_image,
+        title: bookData.title,
+        list_name: bookData.list_name,
+        description: bookData.description,
+        author: bookData.author,
+        buy_links: bookData.buy_links.slice(0, 2),
+      };
+      const markup = markupBook(truncatedBookData);
       openModal();
       addMarkup(modalBodyEl, markup);
-
-      const modalBottomText = document.createElement('p');
-      modalBottomText.textContent = `Congratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.`;
-      modalBottomText.classList.add('modal-bottom-text', 'hidden');
-
-      modalBodyEl.appendChild(modalBottomText);
-
       if (addToShoppingListBtn && addToShoppingListBtn.parentNode) {
         addToShoppingListBtn.parentNode.removeChild(addToShoppingListBtn);
       }
@@ -40,46 +43,39 @@ document.addEventListener('click', async event => {
         ? 'Remove from Shopping List'
         : 'Add to Shopping List';
       addToShoppingListBtn.classList.add('add-to-shopping-list-btn');
-      if (shoppingList.includes(id)) {
-        addToShoppingListBtn.textContent = 'Remove from Shopping List';
-      }
       addToShoppingListBtn.addEventListener('click', () => {
         toggleShoppingList(id);
-        toggleModalBottomText();
-        addToShoppingListBtn.textContent = shoppingList.includes(id)
-          ? 'Remove from Shopping List'
-          : 'Add to Shopping List';
       });
       modalBodyEl.appendChild(addToShoppingListBtn);
+
+      // Зберігаємо інформацію про книгу в local storage
+      localStorage.setItem(id, JSON.stringify(truncatedBookData));
     }
   } catch (error) {
     console.error(error);
   }
 });
 
-function toggleModalBottomText() {
-  const modalBottomText = document.querySelector('.modal-bottom-text');
-  if (modalBottomText) {
-    modalBottomText.classList.toggle('hidden');
-  }
-}
-
 backdrop.addEventListener('click', event => {
   if (event.target === backdrop) {
     closeModal();
   }
 });
+
 document.addEventListener('keydown', event => {
   if (event.key === 'Escape') {
     closeModal();
   }
 });
+
 function openModal() {
   backdrop.style.display = 'block';
 }
+
 function closeModal() {
   backdrop.style.display = 'none';
 }
+
 function markupBook(book) {
   const { book_image, list_name, title, author, description } = book;
   return `<div class="image-container">
@@ -102,6 +98,7 @@ function markupBook(book) {
 function addMarkup(el, markup) {
   el.innerHTML = markup;
 }
+
 function toggleShoppingList(bookId) {
   if (!bookId) {
     console.error('Invalid bookId:', bookId);
@@ -112,14 +109,14 @@ function toggleShoppingList(bookId) {
     shoppingList.push(bookId);
   } else {
     shoppingList.splice(index, 1);
+    // Видаляємо дані про книгу з локального сховища
+    localStorage.removeItem(bookId);
   }
   localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-
+  // Оновлюємо текст кнопки
   if (addToShoppingListBtn) {
-    if (index === -1) {
-      addToShoppingListBtn.textContent = 'Remove from Shopping List';
-    } else {
-      addToShoppingListBtn.textContent = 'Add to Shopping List';
-    }
+    addToShoppingListBtn.textContent = shoppingList.includes(bookId)
+      ? 'Remove from Shopping List'
+      : 'Add to Shopping List';
   }
 }
