27f5881649d1d593aebe1ba4a5473bbe5af81e1f
FedorenkoAnn <144156950+FedorenkoAnn@users.noreply.github.com>
2024-02-28 21:52:39 +0000

    Deploying to gh-pages from @ FedorenkoAnn/StormCodeTeam@64048f623c2c06f77b33f8c0fd73f7b672807191 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/slider-9de199bb.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelector(".shopping-list-container"),o=localStorage.getItem("shoppingList");if(o){const e=JSON.parse(o);c(e,t)}});function c(t,o){t.forEach(e=>{const n=r(e);o.appendChild(n)})}function r(t){const{_id:o,book_image:e,title:n,author:s,description:p}=t,i=document.createElement("div");return i.classList.add("book-item"),i.innerHTML=`
+import"./assets/slider-b337f63a.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelector(".shopping-list-container"),o=localStorage.getItem("shoppingList");if(o){const e=JSON.parse(o);c(e,t)}});function c(t,o){t.forEach(e=>{const n=r(e);o.appendChild(n)})}function r(t){const{_id:o,book_image:e,title:n,author:s,description:p}=t,i=document.createElement("div");return i.classList.add("book-item"),i.innerHTML=`
     <img src="${e}" alt="${n}" />
     <h3>${n}</h3>
     <p>${s}</p>
