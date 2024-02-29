0861da72c61301b02a137976605459a762c38dea
Viktoriia Kucher <142267781+torry455@users.noreply.github.com>
2024-02-29 02:39:55 +0200

    added picture to placeholder

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -3,6 +3,9 @@ import amazon2x from './img/amazon@2x.png';
 import applebooks from './img/app_books@1x.png';
 import applebooks2x from './img/app_books@2x.png';
 import logo from './img/sprite.svg';
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const shoppingListContainer = document.querySelector(
     '.shopping-list-container'
@@ -23,19 +26,32 @@ document.addEventListener('DOMContentLoaded', () => {
     displayPlaceholder(shoppingListContainer);
   }
 });
+
 function displayPlaceholder(container) {
-  const placeholderImageSrc = './img/shopping_books_desk@2x.png'; // Шлях до фото-заглушки
+  const placeholderImageDesk1x = './img/shopping_books_desk@1x.png';
+  const placeholderImageDesk2x = './img/shopping_books_desk@2x.png';
+  const placeholderImageMob1x = './img/shopping_books_mob@1x.png';
+  const placeholderImageMob2x = './img/shopping_books_mob@2x.png';
+  const placeholderImageTab1x = './img/shopping_books_tab@1x.png';
+  const placeholderImageTab2x = './img/shopping_books_tab@2x.png';
+
   const placeholderMarkup = `
     <div class="shopping-list">
-    <h1 class="shopping-list-title">Shopping<span class="shopping-list-span">List</span></h1>
-    <div class="shopping-list-img-book">
-    <p class="shopping-list-paragraf">This page is empty, add some books and proceed to order.</p>
-      <img class="shopping-list-img" src="${placeholderImageSrc}" srcset="${placeholderImageSrc} 2x, ${placeholderImageSrc} 1x" alt="Placeholder Image">
+      <h1 class="shopping-list-title">Shopping<span class="shopping-list-span">List</span></h1>
+      <div class="shopping-list-img-book">
+        <p class="shopping-list-paragraf">This page is empty, add some books and proceed to order.</p>
+        <picture>
+          <source media="(max-width: 575px)" srcset="${placeholderImageMob1x}, ${placeholderImageMob2x} 2x">
+          <source media="(max-width: 991px)" srcset="${placeholderImageTab1x}, ${placeholderImageTab2x} 2x">
+          <source srcset="${placeholderImageDesk1x}, ${placeholderImageDesk2x} 2x">
+          <img class="shopping-list-img" src="${placeholderImageDesk1x}" alt="Placeholder Image">
+        </picture>
       </div>
     </div>
   `;
   container.innerHTML = placeholderMarkup;
 }
+
 function renderShoppingList(shoppingList, container) {
   // Перебираємо елементи shoppingList і відображаємо їх
   shoppingList.forEach((book, index) => {
@@ -54,7 +70,8 @@ function createBookElement(book, index) {
     appleLinks,
     list_name,
   } = book; // Add amazonLinks and appleLinks to destructuring
-  console.log(book);
+  // const bookElementText = document.createElement('h1');
+  // bookElement.classList.add('shopping-list-title');
   const bookElement = document.createElement('div');
   bookElement.classList.add('shopping-list-card-box');
   bookElement.innerHTML = `
@@ -70,14 +87,10 @@ function createBookElement(book, index) {
           : `<p class="shopping-list-card-paragraf">There is no description for this book</p>`
       }
       <div class="shopping-list-card-container-img">
-        <a href="${
-          book.buy_links[0].url
-        }" class="shopping-amazon-link" rel="noopener noreferrer nofollow" target="_blank">
+        <a href="${amazonLinks}" class="shopping-amazon-link" rel="noopener noreferrer nofollow" target="_blank">
           <img class="shopping-list-card-icon-amazon" srcset="${amazon2x} 2x, ${amazon} 1x" src="${amazon}" alt="Amazon" loading="lazy" width="20" height ="20">
         </a>
-        <a href="${
-          book.buy_links[1].url
-        }" class="shopping-book-link" rel="noopener noreferrer nofollow" target="_blank">
+        <a href="${appleLinks}" class="shopping-book-link" rel="noopener noreferrer nofollow" target="_blank">
           <img class="shopping-list-card-icon-book" srcset="${applebooks2x} 2x, ${applebooks} 1x" src="${applebooks}" alt="Apple books" loading="lazy" width="20" height ="20">
         </a>
       </div>
